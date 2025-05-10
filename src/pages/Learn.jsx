e81daa8251e5da96a042a3355ee481842f10e246
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-05-10 18:23:44 -0300

    Add new Div

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,6 +1,7 @@
 // Icons
 import { FaCheck } from "react-icons/fa";
 import { GiOpenBook } from "react-icons/gi";
+import { LuNotebookText } from "react-icons/lu";
 
 // IMG
 import Duolingo from "/img/duolingo.jpg";
@@ -11,38 +12,49 @@ import "./Learn.css";
 
 const Learn = () => {
   return (
-    <div className="learn-container">
-      <img src={DuolingoTwo} className="side-image left" alt="Duolingo" />
-      <div className="buttons">
-        <button>
-          <GiOpenBook />
-        </button>
-        <button>
-          <FaCheck />
-        </button>
-        <button>
-          <GiOpenBook />
-        </button>
-        <button>
-          <FaCheck />
-        </button>
-        <button>
-          <GiOpenBook />
-        </button>
-        <button>
-          <FaCheck />
-        </button>
-        <button>
-          <GiOpenBook />
-        </button>
-        <button>
-          <FaCheck />
-        </button>
-        <button>
-          <GiOpenBook />
-        </button>
+    <div>
+      <div className="sections">
+        <div className="section-text">
+          <h3>Seção 1, Unidade 1</h3>
+          <p>Faça pedido em um café</p>
+        </div>
+        <div className="section-icon">
+          <LuNotebookText />
+        </div>
       </div>
-      <img src={Duolingo} className="side-image right" alt="Duolingo" />
+      <div className="learn-container">
+        <img src={DuolingoTwo} className="side-image left" alt="Duolingo" />
+        <div className="buttons">
+          <button>
+            <GiOpenBook />
+          </button>
+          <button>
+            <FaCheck />
+          </button>
+          <button>
+            <GiOpenBook />
+          </button>
+          <button>
+            <FaCheck />
+          </button>
+          <button>
+            <GiOpenBook />
+          </button>
+          <button>
+            <FaCheck />
+          </button>
+          <button>
+            <GiOpenBook />
+          </button>
+          <button>
+            <FaCheck />
+          </button>
+          <button>
+            <GiOpenBook />
+          </button>
+        </div>
+        <img src={Duolingo} className="side-image right" alt="Duolingo" />
+      </div>
     </div>
   );
 };
