e10a412a3fa02fe62d9c5704004885a0470ba79c
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-05-13 19:31:15 -0300

    Update Store Page

--- src/pages/Pratice.jsx
+++ src/pages/Pratice.jsx
@@ -1,5 +1,5 @@
 // IMG
-import Super from "/img/superText.jpg";
+import Super from "/img/superText.png";
 
 // CSS
 import "./Practice.css";
@@ -7,45 +7,45 @@ import "./Practice.css";
 const Pratice = () => {
   return (
     <div className="practice-container">
-      <h3>Revisão de hoje</h3>
-      <div className="super-card">
+      <h3 className="text-h3">Revisão de hoje</h3>
+      <div className="super-card-first">
         <img src={Super} alt="Suber IMG" />
-        <h3>Relembre a unidade</h3>
-        <p>Revise a unidade 1 pra refrescar a memória!</p>
+        <h3>Escuta só!</h3>
+        <p>Treine o ouvido.</p>
         <button>Desbloquear</button>
       </div>
-      <h3>Conversação</h3>
-      <div className="song-card">
+      <h3 className="text-h3">Conversação</h3>
+      <div className="song-card micro">
         <div className="super-card">
           <h3>Fala</h3>
           <img src={Super} alt="Super IMG" />
         </div>
         <p>Melhore a sua fala com essas frases.</p>
       </div>
-      <div className="song-card">
+      <div className="song-card phone">
         <div className="super-card">
           <h3>Escuta</h3>
           <img src={Super} alt="Super IMG" />
         </div>
         <p>Trabalhe a sua escuta com uma sessão só de áudio.</p>
       </div>
-      <h3>Suas coleções</h3>
-      <div className="song-card">
+      <h3 className="text-h3">Suas coleções</h3>
+      <div className="song-card go-and-back">
         <div className="super-card">
           <h3>Erros</h3>
           <img src={Super} alt="Super IMG" />
         </div>
         <p>Faça uma lição personalizada e revise os seus erros.</p>
       </div>
-      <div className="song-card">
-        <div className="super-card">
+      <div className="song-card carts">
+        <div className="super-card ">
           <h3>Palavras</h3>
           <img src={Super} alt="Super IMG" />
         </div>
         <p>Revise o seu vocabulário de inglês quando quiser.</p>
       </div>
-      <div className="song-card">
-        <div className="super-card">
+      <div className="song-card book">
+        <div className="super-card ">
           <h3>Histórias</h3>
         </div>
         <p>Releia uma história pra revisar palavras em contexto.</p>
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+// IMG
+import Duolingo from "/img/duolingo3.png";
+import Coracao from "/img/coracao.jpg";
+import CoracaoSuper from "/img/coracao-super.jpg";
+import Ofenciva from "/img/gelo.jpg";
+
+// CSS
+import "./Store.css";
 
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <div className="store-container">
+      <div className="store-header">
+        <img src={Duolingo} alt="Duolingo" />
+        <h2>
+          Faça um teste grátis de 2 semanas para aproveitar os benefícios
+          exclusivos do Super!
+        </h2>
+        <button>Começar 14 dias grátis</button>
+      </div>
+      <h3>Vidas</h3>
+      <div className="store-contant">
+        <div className="store-img">
+          <img src={Coracao} alt="Coração Vermelho" />
+        </div>
+        <div className="store-text">
+          <h4>Recuperar vidas</h4>
+          <p>
+            Recupere todas as suas vidas pra se preocupar menos com os erros nas
+            lições.
+          </p>
+          <button className="btn-disabled" disabled>
+            Cheio
+          </button>
+        </div>
+      </div>
+      <div className="store-contant">
+        <div className="store-img">
+          <img src={CoracaoSuper} alt="Coração Colorido" />
+        </div>
+        <div className="store-text">
+          <h4>Vidas ilimitadas</h4>
+          <p>Com o Super, voçê nunca fica sem vidas!</p>
+          <button className="btn-disabled btn-super">Testar Grátis</button>
+        </div>
+      </div>
+      <h3>Superpoderes</h3>
+      <div className="store-contant">
+        <div className="store-img">
+          <img src={Ofenciva} alt="Lagrima Congelada" />
+        </div>
+        <div className="store-text">
+          <h4>Bloqueio de ofensiva</h4>
+          <p>
+            Mantenha a sua ofensiva intacta mesmo se você deixar de praticar por
+            24 horas.
+          </p>
+          <div className="actions">
+            <span>3 Disponíveis</span>
+            <button className="btn-disabled" disabled>
+              Disponível
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Store;
